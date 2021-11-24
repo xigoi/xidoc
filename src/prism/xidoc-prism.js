6a85e999859368c56c2742cf064dc0b78735dacb
Adam Blažek <xigoi@disroot.org>
2021-11-24 17:51:05 +0100

    Syntax highlighting for raw

--- src/prism/xidoc-prism.js
+++ src/prism/xidoc-prism.js
@@ -1,4 +1,32 @@
+const CommandArgs = function(name) {
+  this.name = name;
+  this.pattern = new RegExp("\\[(" + name + ")\\s");
+};
+CommandArgs.prototype.exec = function(str) {
+  const match = this.pattern.exec(str);
+  if (match === null) {
+    return null
+  };
+  const start = match.index + match[0].length;
+  var i = start;
+  for (var unclosedCount = 1; unclosedCount > 0 && i < str.length; i++) {
+    if (str[i] == "[") {
+      unclosedCount++;
+    }
+    if (str[i] == "]") {
+      unclosedCount--;
+    }
+  }
+  if (unclosedCount == 0) {
+    i--;
+  }
+  const end = i;
+  const result = [str.slice(start, end)];
+  result.index = start;
+  return result;
+};
 Prism.languages.xidoc = {
+  string: { pattern: new CommandArgs("raw|pass-raw") },
   tag: { pattern: /(\[)[^\s\]]+/, lookbehind: true },
   punctuation: /[[;\]]/,
 };
