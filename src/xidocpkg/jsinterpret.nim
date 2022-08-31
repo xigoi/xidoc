e25f7a224b21b7ecb6b1f0f9161387edb9c7954c
Adam Blažek <xigoi@disroot.org>
2022-09-01 00:24:16 +0200

    Lazy loading for Prism; xidoc go VROOM

--- src/xidocpkg/jsinterpret.nim
+++ src/xidocpkg/jsinterpret.nim
@@ -1,11 +1,233 @@
 import ./error
+import std/os
 import std/sequtils
 import std/strformat
 import std/strutils
+import std/sugar
+import std/tables
 
 const katexJs = staticRead("../katex/katex.min.js")
-const prismJs = staticRead("../prism/prism.js")
-const xidocPrismJs = staticRead("../prism/xidoc-prism.js")
+const prismCoreJs = staticRead("../prism/prism-core.min.js")
+const prismLanguages = block:
+  var langs: Table[string, string]
+  for file in walkDir("src/prism/languages"):
+    langs[file
+          .path
+          .splitFile
+          .name
+          .dup(removeSuffix(".min"))
+          .dup(removePrefix("prism-"))
+         ] = readFile(file.path)
+  langs
+const prismLangDependencies = {
+  "javascript": @["clike"],
+  "actionscript": @["javascript"],
+  "apex": @["clike", "sql"],
+  "arduino": @["cpp"],
+  "aspnet": @["markup", "csharp"],
+  "birb": @["clike"],
+  "bison": @["c"],
+  "c": @["clike"],
+  "csharp": @["clike"],
+  "cpp": @["c"],
+  "cfscript": @["clike"],
+  "chaiscript": @["clike", "cpp"],
+  "cilkc": @["c"],
+  "cilkcpp": @["cpp"],
+  "coffeescript": @["javascript"],
+  "crystal": @["ruby"],
+  "css-extras": @["css"],
+  "d": @["clike"],
+  "dart": @["clike"],
+  "django": @["markup-templating"],
+  "ejs": @["javascript", "markup-templating"],
+  "etlua": @["lua", "markup-templating"],
+  "erb": @["ruby", "markup-templating"],
+  "fsharp": @["clike"],
+  "firestore-security-rules": @["clike"],
+  "flow": @["javascript"],
+  "ftl": @["markup-templating"],
+  "gml": @["clike"],
+  "glsl": @["c"],
+  "go": @["clike"],
+  "gradle": @["clike"],
+  "groovy": @["clike"],
+  "haml": @["ruby"],
+  "handlebars": @["markup-templating"],
+  "haxe": @["clike"],
+  "hlsl": @["c"],
+  "idris": @["haskell"],
+  "java": @["clike"],
+  "javadoc": @["markup", "java", "javadoclike"],
+  "jolie": @["clike"],
+  "jsdoc": @["javascript", "javadoclike", "typescript"],
+  "js-extras": @["javascript"],
+  "json5": @["json"],
+  "jsonp": @["json"],
+  "js-templates": @["javascript"],
+  "kotlin": @["clike"],
+  "latte": @["clike", "markup-templating", "php"],
+  "less": @["css"],
+  "lilypond": @["scheme"],
+  "liquid": @["markup-templating"],
+  "markdown": @["markup"],
+  "markup-templating": @["markup"],
+  "mongodb": @["javascript"],
+  "n4js": @["javascript"],
+  "objectivec": @["c"],
+  "opencl": @["c"],
+  "parser": @["markup"],
+  "php": @["markup-templating"],
+  "phpdoc": @["php", "javadoclike"],
+  "php-extras": @["php"],
+  "plsql": @["sql"],
+  "processing": @["clike"],
+  "protobuf": @["clike"],
+  "pug": @["markup", "javascript"],
+  "purebasic": @["clike"],
+  "purescript": @["haskell"],
+  "qsharp": @["clike"],
+  "qml": @["javascript"],
+  "qore": @["clike"],
+  "racket": @["scheme"],
+  "cshtml": @["markup", "csharp"],
+  "jsx": @["markup", "javascript"],
+  "tsx": @["jsx", "typescript"],
+  "reason": @["clike"],
+  "ruby": @["clike"],
+  "sass": @["css"],
+  "scss": @["css"],
+  "scala": @["java"],
+  "shell-session": @["bash"],
+  "smarty": @["markup-templating"],
+  "solidity": @["clike"],
+  "soy": @["markup-templating"],
+  "sparql": @["turtle"],
+  "sqf": @["clike"],
+  "squirrel": @["clike"],
+  "stata": @["mata", "java", "python"],
+  "t4-cs": @["t4-templating", "csharp"],
+  "t4-vb": @["t4-templating", "vbnet"],
+  "tap": @["yaml"],
+  "tt2": @["clike", "markup-templating"],
+  "textile": @["markup"],
+  "twig": @["markup-templating"],
+  "typescript": @["javascript"],
+  "v": @["clike"],
+  "vala": @["clike"],
+  "vbnet": @["basic"],
+  "velocity": @["markup"],
+  "wiki": @["markup"],
+  "xeora": @["markup"],
+  "xml-doc": @["markup"],
+  "xquery": @["markup"],
+}.toTable
+const prismLangAliases = {
+  "html": "markup",
+  "xml": "markup",
+  "svg": "markup",
+  "mathml": "markup",
+  "ssml": "markup",
+  "atom": "markup",
+  "rss": "markup",
+  "js": "javascript",
+  "g4": "antlr4",
+  "ino": "arduino",
+  "arm-asm": "armasm",
+  "art": "arturo",
+  "adoc": "asciidoc",
+  "avs": "avisynth",
+  "avdl": "avro-idl",
+  "gawk": "awk",
+  "sh": "bash",
+  "shell": "bash",
+  "shortcode": "bbcode",
+  "rbnf": "bnf",
+  "oscript": "bsl",
+  "cs": "csharp",
+  "dotnet": "csharp",
+  "cfc": "cfscript",
+  "cilk-c": "cilkc",
+  "cilk-cpp": "cilkcpp",
+  "cilk": "cilkcpp",
+  "coffee": "coffeescript",
+  "conc": "concurnas",
+  "jinja2": "django",
+  "dns-zone": "dns-zone-file",
+  "dockerfile": "docker",
+  "gv": "dot",
+  "eta": "ejs",
+  "xlsx": "excel-formula",
+  "xls": "excel-formula",
+  "gamemakerlanguage": "gml",
+  "po": "gettext",
+  "gni": "gn",
+  "ld": "linker-script",
+  "go-mod": "go-module",
+  "hbs": "handlebars",
+  "mustache": "handlebars",
+  "hs": "haskell",
+  "idr": "idris",
+  "gitignore": "ignore",
+  "hgignore": "ignore",
+  "npmignore": "ignore",
+  "webmanifest": "json",
+  "kt": "kotlin",
+  "kts": "kotlin",
+  "kum": "kumir",
+  "tex": "latex",
+  "context": "latex",
+  "ly": "lilypond",
+  "emacs": "lisp",
+  "elisp": "lisp",
+  "emacs-lisp": "lisp",
+  "md": "markdown",
+  "moon": "moonscript",
+  "n4jsd": "n4js",
+  "nani": "naniscript",
+  "objc": "objectivec",
+  "qasm": "openqasm",
+  "objectpascal": "pascal",
+  "px": "pcaxis",
+  "pcode": "peoplecode",
+  "plantuml": "plant-uml",
+  "pq": "powerquery",
+  "mscript": "powerquery",
+  "pbfasm": "purebasic",
+  "purs": "purescript",
+  "py": "python",
+  "qs": "qsharp",
+  "rkt": "racket",
+  "razor": "cshtml",
+  "rpy": "renpy",
+  "res": "rescript",
+  "robot": "robotframework",
+  "rb": "ruby",
+  "sh-session": "shell-session",
+  "shellsession": "shell-session",
+  "smlnj": "sml",
+  "sol": "solidity",
+  "sln": "solution-file",
+  "rq": "sparql",
+  "sclang": "supercollider",
+  "t4": "t4-cs",
+  "trickle": "tremor",
+  "troy": "tremor",
+  "trig": "turtle",
+  "ts": "typescript",
+  "tsconfig": "typoscript",
+  "uscript": "unrealscript",
+  "uc": "unrealscript",
+  "url": "uri",
+  "vb": "visual-basic",
+  "vba": "visual-basic",
+  "webidl": "web-idl",
+  "mathematica": "wolfram",
+  "nb": "wolfram",
+  "wl": "wolfram",
+  "xeoracube": "xeora",
+  "yml": "yaml"
+}.toTable
 
 when defined(js):
 
@@ -20,20 +242,6 @@ when defined(js):
     opts["throwOnError"] = false
     opts["displayMode"] = displayMode
     $katexRenderToString(math.cstring, opts)
-
-  # This doesn't compile for a bizarre reason
-  #
-  # {.emit: prismJs.}
-  # {.emit: "window.Prism={manual:true};"}
-  #
-  # var Prism {.importjs.}: JsObject
-  # proc prismHighlight(code: cstring, lang: JsObject, langStr: cstring): cstring {.importjs: "Prism.highlight(@)"}
-  #
-  # proc highlightCode*(code: string, lang: string): string =
-  #   try:
-  #     $prismHighlight(code, Prism.languages[lang], lang)
-  #   except:
-  #     xidocError &"Unknown language: {lang}"
 
   proc highlightCode*(code: string, lang: string): string =
     code
@@ -143,22 +351,34 @@ else:
       xidocError &"Error while rendering math: {math}\n{ctx.exceptionMsg}"
     return ctx.toString(res)
 
+  proc loadPrismLanguage(name: string) =
+    var loaded {.global.}: seq[string]
+    let name =
+      if name in prismLanguages: name
+      elif name in prismLangAliases: prismLangAliases[name]
+      else: xidocError &"Unknown language for syntax highlighting: {name}"
+    if name in loaded: return
+    for dep in prismLangDependencies.getOrDefault(name, @[]):
+      loadPrismLanguage(dep)
+    ctx.free(ctx.eval(prismLanguages[name]))
+    loaded.add(name)
+
   proc highlightCode*(code: string, lang: string): string =
     var highlight {.global.}: JsValue
     var languages {.global.}: JsValue
     once:
       initCtx()
-      ctx.free(ctx.eval(prismJs))
-      ctx.free(ctx.eval(xidocPrismJs))
+      ctx.free(ctx.eval(prismCoreJs))
       highlight = ctx.eval("Prism.highlight")
       languages = ctx.eval("Prism.languages")
       addExitProc do():
         ctx.free(highlight)
         ctx.free(languages)
+    loadPrismLanguage(lang)
     let language = ctx.getProperty(languages, lang)
     defer: ctx.free(language)
     if isUndefined(language):
-      xidocError &"Unknown language for syntax highlighting: {lang}"
+      xidocError &"Error loading language for syntax highlighting: {lang}"
     var args = [ctx.toJs(code), language, ctx.toJs(lang)]
     defer:
       ctx.free(args[0])
@@ -175,7 +395,7 @@ else:
     defer: ctx.free(functionJs)
     if not ctx.isFunction(functionJs):
       xidocError &"Invalid JavaScript function: {function}\n{ctx.exceptionMsg}"
-    var args = args.mapIt(ctx.toJS(it.cstring))
+    var args = collect(for arg in args: ctx.toJs(arg.cstring))
     defer: ctx.free(args)
     let res = ctx.call(functionJs, undefined, args.len.cint, args[0].addr)
     defer: ctx.free(res)
